6cef46702f8d8e45e9ec20d7e5989bc4f1b5c146
saurabh-chamola <sourabhchamola5@gmail.com>
2025-07-22 09:51:23 -0700

    scnd commit

--- my-tailwind-app/vite.config.js
+++ my-tailwind-app/vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 1000, // Poll every 1000ms (1s) — you can reduce if needed
+    },
+  },
 })
